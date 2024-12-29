568ea9528d63bd60d2e79c4cd1d60091c4d1a495
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2024-12-29 10:23:46 +0100

    ok

--- server/src/database.rs
+++ server/src/database.rs
@@ -5,8 +5,19 @@ use anyhow::Context as _;
 
 pub static CONNECTION_POOL: OnceCell<PgPool> = OnceCell::const_new();
 
-/// Init the database with tables if they don't exist
-async fn init_db(pool: &PgPool) -> anyhow::Result<()> {
+/// Initialize a connection pool to the database
+fn database_url() -> anyhow::Result<String> {
+    let user = env::var("POSTGRES_USER")?;
+    let password = env::var("POSTGRES_PASSWORD")?;
+    let host = env::var("POSTGRES_HOST")?;
+    let port = env::var("POSTGRES_PORT")?;
+    let database = env::var("POSTGRES_DB")?;
+    let url = format!("postgres://{user}:{password}@{host}:{port}/{database}");
+    Ok(url)
+}
+
+/// Initialize the database with tables if they don't exist
+async fn init_database(pool: &PgPool) -> anyhow::Result<()> {
     sqlx::query(
         "CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY,
@@ -16,21 +27,17 @@ async fn init_db(pool: &PgPool) -> anyhow::Result<()> {
     Ok(())
 }
 
-/// Get a connection pool to the database
+/// Initialize a connection pool to the database
 async fn init_connection_pool() -> anyhow::Result<PgPool> {
-    let user = env::var("POSTGRES_USER")?;
-    let password = env::var("POSTGRES_PASSWORD")?;
-    let host = env::var("POSTGRES_HOST")?;
-    let port = env::var("POSTGRES_PORT")?;
-    let database = env::var("POSTGRES_DB")?;
-    let url = format!("postgres://{user}:{password}@{host}:{port}/{database}");
+    let url = database_url()?;
     let pool = PgPool::connect(&url).await?;
     // Create the tables if they don't exist
-    init_db(&pool).await?;
+    init_database(&pool).await?;
     Ok(pool)
 }
 
-pub async fn get_connection_pool() -> anyhow::Result<&'static PgPool> {
+/// Get the connection pool
+pub async fn connection_pool() -> anyhow::Result<&'static PgPool> {
     Ok(CONNECTION_POOL.get_or_try_init(init_connection_pool).await?)
 }
 
