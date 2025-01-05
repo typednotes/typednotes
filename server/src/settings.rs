c950c9c4c75c9e64798467ab82d2a9cfc81acc5a
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2025-01-05 22:51:17 +0100

    ok

--- server/src/settings.rs
+++ server/src/settings.rs
@@ -1,51 +1,37 @@
 use std::env;
-use config::{Config, ConfigError, Environment, File};
+use config::{Config, ConfigError, Environment, File, FileFormat};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 #[allow(unused)]
 struct Database {
     user: String,
+    password: String,
     host: String,
     port: String,
     database: String,
-    url: String,
+}
+
+impl Database {
+    
 }
 
 #[derive(Debug, Deserialize)]
 #[allow(unused)]
 pub(crate) struct Settings {
     database: Database,
-    
 }
 
 impl Settings {
     pub(crate) fn new() -> Result<Self, ConfigError> {
-        let run_mode = env::var("RUN_MODE").unwrap_or_else(|_| "development".into());
-
         let s = Config::builder()
-            // Start off by merging in the "default" configuration file
-            .add_source(File::with_name("examples/hierarchical-env/config/default"))
-            // Add in the current environment file
-            // Default to 'development' env
-            // Note that this file is _optional_
-            .add_source(
-                File::with_name(&format!("examples/hierarchical-env/config/{run_mode}"))
-                    .required(false),
-            )
-            // Add in a local configuration file
-            // This file shouldn't be checked in to git
-            .add_source(File::with_name("examples/hierarchical-env/config/local").required(false))
-            // Add in settings from the environment (with a prefix of APP)
-            // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
-            .add_source(Environment::with_prefix("app"))
-            // You may also programmatically change settings
-            .set_override("database.url", "postgres://")?
+            .set_default("user", "typednotes")?
+            .set_default("password", "password")?
+            .set_default("host", "localhost")?
+            .set_default("port", "5432")?
+            .set_default("database", "typednotes")?
+            .add_source(File::with_name("config").format(FileFormat::Toml).required(false))
             .build()?;
-
-        // Now that we're done, let's access our configuration
-        println!("debug: {:?}", s.get_bool("debug"));
-        println!("database: {:?}", s.get::<String>("database.url"));
 
         // You can deserialize (and thus freeze) the entire configuration as
         s.try_deserialize()
